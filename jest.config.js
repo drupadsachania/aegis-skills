2973c04db57977444c9e57f8be113e33ba02975c
Drupad Sachania <drupad.h.sachania@live.com>
2026-05-23 12:06:20 +0530

    chore: add Tailwind CSS and React Testing Library

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  }
+})
